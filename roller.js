1fc06cab6bc871adb1ecaa6c295c5de062985a1e
achesak <achesak@yahoo.com>
2017-08-22 08:57:27 -0400

    Add new fields; styling

--- roller.js
+++ roller.js
@@ -1,45 +1,87 @@
-let roller = function(count, die, critRange, critMultipler, ac, dr, health) {
+let roller = function(count, die, critRange, critMultipler, ac, dr, health, atk, recoil) {
+
     let output = [];
-    var damage = 0;
+    let damage = 0;
+    let hits = 0;
+    let sub = atk;
+
     for (let i = 0; i < count; i++) {
-        let roll = Math.floor(Math.random() * 20) + 1;
-        if (roll == 1) {
-            output.push("Roll " + (i + 1) + ": CRITICAL FAIL! " + damage + " total.");
+
+        if (damage >= health) {
+            output.push("<span class='dead'>Enemy dead! " + damage + " total.</span>");
             break;
+        }
+
+        if (i == 0) {
+            sub = atk;
+        } else {
+            sub = recoil;
+        }
+
+        let roll = Math.floor(Math.random() * 20) + 1;
+        let rollBase = roll;
+
+        if (roll == 1) {
+
+            output.push("<span class='fail'>Roll " + (i + 1) + ": CRITICAL FAIL! " + damage + " total.</span>");
+            break;
+
         } else if (roll >= critRange) {
-            output.push("Roll " + (i + 1) + ": CRITICAL (" + roll + ")! Rolling to confirm...");
+
+            output.push("<span class='crit1'>Roll " + (i + 1) + ": CRITICAL (" + roll + ")! Rolling to confirm...</span>");
+            hits++;
             let roll2 = Math.floor(Math.random() * 20) + 1;
+            let roll2Base = roll2;
+
             if (roll2 >= critRange) {
-                output.push("Roll " + (i + 1) + ": DOUBLE CRITICAL (" + roll2 + ")! Rolling to confirm...");
+
+                output.push("<span class='crit2'>Roll " + (i + 1) + ": DOUBLE CRITICAL (" + roll2 + ")! Rolling to confirm...</span>");
                 let roll3 = Math.floor(Math.random() * 20) + 1;
+
                 if (roll3 >= critRange) {
-                    output.push("Roll " + (i + 1) + ": TRIPLE CRITICAL (" + roll3 + ")! INSTANT KILL!");
+                    output.push("<span class='crit3'>Roll " + (i + 1) + ": TRIPLE CRITICAL (" + roll3 + ")! INSTANT KILL!</span>");
                     break;
-                } else {
-                    let rollDamage = Math.max(0, ((Math.floor(Math.random() * die) + 1) * critMultipler) - dr);
-                    damage += rollDamage;
-                    output.push("Roll " + (i + 1) + ": CRITICAL CONFIRMED (" + roll3 + ")! " + rollDamage + " damage, " + damage + " total.");
                 }
-            } else if (roll2 >= ac) {
+
                 let rollDamage = Math.max(0, ((Math.floor(Math.random() * die) + 1) * critMultipler) - dr);
                 damage += rollDamage;
-                output.push("Roll " + (i + 1) + ": CRITICAL CONFIRMED (" + roll2 + ")! " + rollDamage + " damage, " + damage + " total.");
+                output.push("<span class='crit2'>Roll " + (i + 1) + ": CRITICAL CONFIRMED (" + roll2 + ")! " + rollDamage + " damage, " + damage + " total.</span>");
+                continue;
+            }
+
+            roll2 += sub;
+
+            if (roll2 >= ac) {
+
+                let rollDamage = Math.max(0, ((Math.floor(Math.random() * die) + 1) * critMultipler) - dr);
+                damage += rollDamage;
+                output.push("<span class='crit1'>Roll " + (i + 1) + ": CRITICAL CONFIRMED (" + roll2Base + " + " + sub + " = " + roll2 + ")! " + rollDamage + " damage, " + damage + " total.</span>");
+                continue;
+
             } else {
+
                 let rollDamage = Math.max(0, Math.floor(Math.random() * die) + 1 - dr);
                 damage += rollDamage;
-                output.push("Roll " + (i + 1) + ": No critical (" + roll2 + ")! " + rollDamage + " damage, " + damage + " total.");
+                output.push("<span class='crit1'>Roll " + (i + 1) + ": No critical (" + roll2Base + " + " + sub + " = " + roll2 + ")! " + rollDamage + " damage, " + damage + " total.</span> ");
+                continue;
+
             }
-        } else if (roll >= ac) {
+        }
+
+        roll += sub;
+
+        if (roll >= ac) {
+
             let rollDamage = Math.max(0, Math.floor(Math.random() * die) + 1 - dr);
             damage += rollDamage;
-            output.push("Roll " + (i + 1) + ": Hit (" + roll + "). " + rollDamage + " damage, " + damage + " total.");
+            hits++;
+            output.push("<span class='hit'>Roll " + (i + 1) + ": Hit (" + rollBase + " + " + sub + " = " + roll + "). " + rollDamage + " damage, " + damage + " total.</span>");
+
         } else {
-            output.push("Roll " + (i + 1) + ": Miss (" + roll + ").");
-        }
-        if (damage >= health) {
-            output.push("Enemy dead! " + damage + " total.");
-            break;
+            output.push("<span class='miss'>Roll " + (i + 1) + ": Miss (" + rollBase + " + " + sub + " = " + roll + ").</span>");
         }
     }
-    return [output, damage];
+
+    output.push("Final stats: " + hits + " hits, " + damage + " damage, " + Math.max(0, health - damage) + " health left.");
+    return output;
 };
